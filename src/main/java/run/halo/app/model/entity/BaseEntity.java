4125ee65a2bc44183aa6eb33b03fb53e8aa4d8ad
supersu0 <51224786+supersu0@users.noreply.github.com>
2019-10-25 00:49:25 +0800

    Create BaseEntity.java

---
+++ src/main/java/run/halo/app/model/entity/BaseEntity.java
@@ -0,0 +1,9 @@
+package run.halo.app.model.entity;
+import run.halo.app.utils.DateUtils;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import javax.persistence.*;
+import java.util.Date;
+
+//Base entity
