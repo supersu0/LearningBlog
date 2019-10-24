c91e0c1eb93b78841d72bcc8c3d15308000ccd5d
supersu0 <51224786+supersu0@users.noreply.github.com>
2019-10-24 21:50:46 +0800

    Create Log.java
    注解了Log.java的包

---
+++ src/main/java/run/halo/app/model/entity/Log.java
@@ -0,0 +1,9 @@
+package run.halo.app.model.entity;
+import lombok.Data
+//https://www.jianshu.com/p/c1ee7e4247bf  @Data注解 与 lombok
+//https://blog.csdn.net/zhanlanmg/article/details/50392266  lombok @EqualsAndHashCode 注解的影响
+import lombok.EqualsAndHashCode
+import lombok.ToString;
+import run.app.model.enums.AttachmentType;
+import javax.persistence.*;
+//https://www.jianshu.com/p/afd588c8951c  对象持久化 API 之 JPA 入门教程
